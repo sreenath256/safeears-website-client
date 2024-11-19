6b5c9eaf8f21694abdd58a0e2030bc72f165e62c
Rashid <rashidpk090@gmail.com>
2024-11-19 20:00:31 +0530

    adsf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import { BsCart4 } from "react-icons/bs";
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { MdClose } from 'react-icons/md';
-import CartModal from '../components/Cartmodal';
+import CartModal from './CartModal';
 
 const menuItems=[
   {title:`home`,url:`/`},
